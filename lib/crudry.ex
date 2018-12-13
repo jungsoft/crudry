fdb71c6f7b6fbb0e154b8cb0e63cf9c11347d99d
Gabriel <gabrielpra1@gmail.com>
2018-12-13 16:14:14 -0500

    Create repository

---
+++ lib/crudry.ex
@@ -0,0 +1,18 @@
+defmodule Crudry do
+  @moduledoc """
+  Documentation for Crudry.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Crudry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crudry_test.exs
@@ -0,0 +1,8 @@
+defmodule CrudryTest do
+  use ExUnit.Case
+  doctest Crudry
+
+  test "greets the world" do
+    assert Crudry.hello() == :world
+  end
+end
