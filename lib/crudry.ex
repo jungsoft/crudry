dd6bbece93084a4112e7063a11a30594389e1a70
Gabriel <gabrielpra1@gmail.com>
2018-12-15 13:31:22 -0500

    Allow definition of custom changeset functions for create and update

--- lib/crudry.ex
+++ lib/crudry.ex
@@ -1,10 +1,32 @@
 defmodule Crudry do
   @moduledoc """
-  Documentation for Crudry.
+  Crudry is a library for DRYing CRUD.
   """
 
-  defmacro define_functions(opts) do
-    module = get_module(opts)
+  # Isn't required, only used to update default changeset functions
+  # If it isn't needed, should it be a normal macro instead of __using__?
+  defmacro __using__(opts) do
+    Module.put_attribute(__CALLER__.module, :create_changeset, opts[:create])
+    Module.put_attribute(__CALLER__.module, :update_changeset, opts[:update])
+
+    # This is for making it available at run-time. But is it needed?
+    quote bind_quoted: [opts: opts] do
+      @create_changeset opts[:create]
+      @update_changeset opts[:update]
+    end
+  end
+
+  defmacro create_functions(schema_module, opts \\ []) do
+    create_changeset = Module.get_attribute(__CALLER__.module, :create_changeset)
+    update_changeset = Module.get_attribute(__CALLER__.module, :update_changeset)
+
+    # Default changeset is defined in `use` or `changeset` if nothing is defined.
+    opts = Keyword.merge(
+      [create: create_changeset || :changeset,
+       update: update_changeset || :changeset],
+       opts)
+
+    module = get_module(schema_module)
     name = get_module_name(module) |> String.downcase()
 
     quote location: :keep do
@@ -26,13 +48,13 @@ defmodule Crudry do
       def unquote(:"create_#{name}")(attrs) do
         unquote(module)
         |> struct()
-        |> unquote(module).changeset(attrs)
+        |> unquote(module).unquote(opts[:create])(attrs)
         |> alias!(Repo).insert()
       end
 
       def unquote(:"update_#{name}")(%module{} = struct, attrs) do
         struct
-        |> unquote(module).changeset(attrs)
+        |> unquote(module).unquote(opts[:update])(attrs)
         |> alias!(Repo).update()
       end
 
@@ -55,10 +77,14 @@ defmodule Crudry do
     end
   end
 
+  # Transforms an AST representation like {:__aliases__, [alias: false], [:Module, :Submodule]}
+  # into Module.Submodule
   defp get_module(opts) do
     Macro.expand(opts, __ENV__)
   end
 
+  # Get the name of the module as a string.
+  # Transforms Module.Submodule into "Submodule"
   defp get_module_name(module) do
     module
     |> to_string()
--- test/crudry_test.exs
+++ test/crudry_test.exs
@@ -2,7 +2,7 @@ defmodule CrudryTest do
   use ExUnit.Case
   doctest Crudry
 
-  # Mock for Repo.
+  # Context for Repo.
   defmodule Repo do
     def insert(changeset) do
       {:ok, changeset}
@@ -30,30 +30,89 @@ defmodule CrudryTest do
     end
   end
 
-  # Mock for a schema
+  # Context for a schema
   defmodule Test do
     defstruct x: "123", bang: false
 
+    # Each changeset functions changes `attrs` in a different way so
+    # we can verify which one was called.
+    # TODO: This is not a clean way to do it, so change it
+
     def changeset(test, attrs) do
+      Map.merge(test, attrs)
+    end
+
+    def create_changeset(test, %{x: x}) do
+      attrs = %{x: x + 1}
+      Map.merge(test, attrs)
+    end
+
+    def update_changeset(test, %{x: x}) do
+      attrs = %{x: x + 2}
       Map.merge(test, attrs)
     end
   end
 
   # Mock for a context
-  defmodule Mock do
+  defmodule Context do
     alias CrudryTest.Repo
 
-    Crudry.define_functions CrudryTest.Test
+    Crudry.create_functions CrudryTest.Test
   end
 
   test "creates the CRUD functions" do
-    assert Mock.create_test(%{x: 2}) == {:ok, %Test{x: 2}}
-    assert Mock.list_tests() == [1, 2, 3]
-    assert Mock.get_test(1) == %Test{x: "123"}
-    assert Mock.get_test!(3) == %Test{x: "123", bang: true}
-    assert Mock.update_test(struct(Test), %{x: 3}) == {:ok, %Test{x: 3}}
-    assert Mock.update_test(3, %{x: 3}) == {:ok, %Test{x: 3}}
-    assert Mock.delete_test(struct(Test)) == :deleted
-    assert Mock.delete_test(2) == :deleted
+    assert Context.create_test(%{x: 2}) == {:ok, %Test{x: 2}}
+    assert Context.list_tests() == [1, 2, 3]
+    assert Context.get_test(1) == %Test{x: "123"}
+    assert Context.get_test!(3) == %Test{x: "123", bang: true}
+    assert Context.update_test(struct(Test), %{x: 3}) == {:ok, %Test{x: 3}}
+    assert Context.update_test(3, %{x: 3}) == {:ok, %Test{x: 3}}
+    assert Context.delete_test(struct(Test)) == :deleted
+    assert Context.delete_test(2) == :deleted
+  end
+
+  test "allow defining of create changeset" do
+    defmodule ContextCreate do
+      alias CrudryTest.Repo
+
+      Crudry.create_functions CrudryTest.Test, create: :create_changeset
+    end
+
+    assert ContextCreate.create_test(%{x: 2}) == {:ok, %Test{x: 3}}
+    assert ContextCreate.update_test(struct(Test), %{x: 2}) == {:ok, %Test{x: 2}}
+  end
+
+  test "allow defining of update changeset" do
+    defmodule ContextUpdate do
+      alias CrudryTest.Repo
+
+      Crudry.create_functions CrudryTest.Test, update: :update_changeset
+    end
+
+    assert ContextUpdate.create_test(%{x: 2}) == {:ok, %Test{x: 2}}
+    assert ContextUpdate.update_test(struct(Test), %{x: 2}) == {:ok, %Test{x: 4}}
+  end
+
+  test "allow defining of both changeset functions" do
+    defmodule ContextBoth do
+      alias CrudryTest.Repo
+
+      Crudry.create_functions CrudryTest.Test, create: :create_changeset, update: :update_changeset
+    end
+
+    assert ContextBoth.create_test(%{x: 2}) == {:ok, %Test{x: 3}}
+    assert ContextBoth.update_test(struct(Test), %{x: 2}) == {:ok, %Test{x: 4}}
+  end
+
+  test "allow defining default changeset functions for context" do
+    defmodule ContextDefault do
+      alias CrudryTest.Repo
+
+      use Crudry, create: :create_changeset, update: :update_changeset
+      Crudry.create_functions CrudryTest.Test
+    end
+
+    assert ContextDefault.create_test(%{x: 2}) == {:ok, %Test{x: 3}}
+    assert ContextDefault.update_test(struct(Test), %{x: 2}) == {:ok, %Test{x: 4}}
   end
 end
