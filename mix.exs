aaa6323e876f0e14f8914b98309c070467d5eb61
Gabriel Prá <gabrielpra1@gmail.com>
2020-09-14 12:30:11 -0300

    Bump version to 2.3.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Crudry.MixProject do
   def project do
     [
       app: :crudry,
-      version: "2.2.1",
+      version: "2.3.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       compilers: [:gettext] ++ Mix.compilers,
